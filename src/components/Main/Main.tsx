141f5f6a1eff064a3083174d59f3112380fd7c00
Leonardo Manrique <leonardomanrique43@gmail.com>
2025-09-24 03:07:31 +0200

    Remove unused imports and commented code in Main
    Cleaned up Main.tsx by removing the unused Services import and commented-out sections. Also updated App import in main.tsx to remove file extension for consistency.

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Home from '../Home/Home'
 import About from '../About/About'
-import Services from '../Services/Services'
 import Qualification from '../Qualification/Qualification'
 
 const Main: React.FC = () => {
@@ -13,31 +12,8 @@ const Main: React.FC = () => {
       {/* ==================== ABOUT ==================== */}
       <About />
 
-      {/* ==================== SERVICES ==================== */}
-        {/*<Services />*/}
-
       {/* ==================== QUALIFICATION ==================== */}
       <Qualification />
-
-      {/* ==================== PORTFOLIO ==================== */}
-      {/* <section className="portfolio section" id="portfolio">
-
-      </section> */}
-
-      {/* ==================== PROJECT IN MIND ==================== */}
-      {/* <section className="project section">
-
-      </section> */}
-
-      {/* ==================== TESTIMONIAL ==================== */}
-      {/* <section className="testimonial section">
-
-      </section> */}
-
-      {/* ==================== CONTACT ME ==================== */}
-      {/* <section className="contact section" id="contact">
-
-      </section> */}
     </main>
   )
 }
